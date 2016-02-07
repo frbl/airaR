d73eb8cd5677d8b765275c04777a1bf769d73359
Frank Blaauw <frank.blaauw@gmail.com>
2016-02-07 15:32:34 +0100

    Trying to get the package to work

--- tests/testthat/test_aira_output.R
+++ tests/testthat/test_aira_output.R
@@ -1,7 +1,7 @@
 context('aira_output')
 
 testdata_aira_model <- function() {
-  data_set <- autovar::read_spss("../../pp1 nieuw compleet.sav", to.data.frame=TRUE)
+  data_set <- autovar::read_spss("../../inst/pp1_nieuw_compleet.sav", to.data.frame=TRUE)
   endodata <- data_set[,c('SomBewegUur', 'SomPHQ')]
   exodata <- data_set[,c('UitbijterPHQ','UitbijterBeweg')]
   var_model <- vars::VAR(endodata, exogen=exodata, p=2, type='const')
@@ -25,30 +25,81 @@ test_that('print_percentage_effect', {
   expect_equal(result, expected)
 })
 
+test_that('export_model', {
+  json_example ='{
+  "links": [
+  {
+  "source": 0,
+  "target": 1,
+  "distance": 0.9,
+  "weight": "-0.258619869521396"
+  }
+  ],
+  "nodes": [
+  {
+  "index": 0,
+  "name": "SomBewegUur",
+  "key": "SomBewegUur",
+  "val": -1.8597041
+  },
+  {
+  "index": 1,
+  "name": "SomPHQ",
+  "key": "SomPHQ",
+  "val": 0.1706285
+  }
+  ]
+}'
+
+  edges <- data.frame(source = 0,
+                      target = 1,
+                      distance = 0.9,
+                      weight = "-0.258619869521396",
+                      stringsAsFactors=FALSE
+                      )
+
+  nodes <- data.frame(index = c(0, 1),
+                      name = c('SomBewegUur', 'SomPHQ'),
+                      key = c('SomBewegUur', 'SomPHQ'),
+                      val = c(-1.8597041, 0.1706285),
+                      stringsAsFactors=FALSE
+  )
+  expected <- list(links = edges, nodes = nodes)
+
+  aira_output <- AiraOutput$new(aira = testdata_aira_model())
+  result <- aira_output$export_model()
+  expect_equal(result, expected, tolerance=1e-5)
+})
+
 test_that('export_model_to_json', {
   json_example ='{
-  "nodes": [
+  "links": [
     {
-      "name": "SomPHQ",
-      "key": "SomPHQ",
-      "val": 3
-    }, {
-      "name": "SomBewegUur",
-      "key": "SomBewegUur",
-      "val": 3
+      "source": 0,
+      "target": 1,
+      "distance": 0.9,
+      "weight": "-0.258619869521396"
     }
   ],
-  "links": [{
-    "weight": 0.1,
-    "distance": 0.9,
-    "source": 0,
-    "target": 1
-  }, {
-    "weight": 0.1,
-    "distance": 0.9,
-    "source": 1,
-    "target": 0
-  }]
+  "nodes": [
+    {
+      "index": 0,
+      "name": "SomBewegUur",
+      "key": "SomBewegUur",
+      "val": -1.8597041
+    },
+    {
+      "index": 1,
+      "name": "SomPHQ",
+      "key": "SomPHQ",
+      "val": 0.1706285
+    }
+  ]
 }'
-x <- jsonlite::fromJSON(json_example)
+
+  # remove newlines
+  expected <- jsonlite::toJSON(jsonlite::fromJSON(json_example))
+  aira_output <- AiraOutput$new(aira = testdata_aira_model())
+  result <- aira_output$export_model_to_json()
+  expect_equal(result, expected)
 })
