aa5f2fe4ec490e22a040d90d82f258fbf184a4a1
Frank Blaauw <frank.blaauw@gmail.com>
2016-02-09 23:12:55 +0100

    moved override function to separate file

--- R/convert_var_to_positive.R
+++ R/convert_var_to_positive.R
@@ -1,3 +1,8 @@
+#' Converts a var model to a positive notation, returns the updated var model
+#'
+#' @param var_model the var model that should be converted to positive
+#' @param negative_variables the variables that are negative, and should be positivised
+#' @return the positivised var-model
 #' @export
 convert_var_to_positive <- function(var_model, negative_variables) {
   # create a matrix of ones
