faaa40c9d5046a3a8266a34734ce0a38790ca42c
Frank Blaauw <frank.blaauw@gmail.com>
2016-02-15 09:52:27 +0100

    Updated the positivity method and the output function for networks

--- inst/file-test.R
+++ inst/file-test.R
@@ -1,4 +1,5 @@
 library('aira')
+library('vars')
 data_set <- autovar::read_spss("inst/pp1_nieuw_compleet.sav", to.data.frame=TRUE)
 endodata <- data_set[,c('SomBewegUur', 'SomPHQ')]
 exogedata <- data_set[,c('UitbijterPHQ','UitbijterBeweg')]
@@ -6,16 +7,23 @@ exogedata <- data_set[,c('UitbijterPHQ','UitbijterBeweg')]
 
 var.2c <- vars::VAR(endodata, p=2, type='const', exogen=exogedata)
 negative_variables <- c('SomPHQ')
-aira <- Aira$new(bootstrap_iterations = 0, horizon= 10, var_model = var.2c, orthogonalize= TRUE)
-tot <- aira$determine_best_node_from_all()
-print(tot)
-
-var.2c <-convert_var_to_positive(var.2c, negative_variables = negative_variables)
 
 aira <- Aira$new(bootstrap_iterations = 0, horizon= 10, var_model = var.2c, orthogonalize= TRUE)
 tot <- aira$determine_best_node_from_all()
 print(tot)
 
+norm <- irf(var.2c, ortho = FALSE, boot= FALSE)
+var.2c.pos <-convert_var_to_positive(var.2c, negative_variables = negative_variables)
+pos <- irf(var.2c.pos, ortho = FALSE, boot = FALSE)
+
+aira <- Aira$new(bootstrap_iterations = 0, horizon= 10, var_model = var.2c.pos, orthogonalize= TRUE)
+tot <- aira$determine_best_node_from_all()
+print(tot)
+
+print(norm$irf$SomBewegUur)
+print(pos$irf$SomBewegUur)
+print(Bcoef(var.2c))
+print(Bcoef(new))
 
 # Shock Stress
 # Shock Activity
