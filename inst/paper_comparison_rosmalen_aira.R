380f8b7585685fee1ef32b1be4e23d15a4081a73
Frank Blaauw <frank.blaauw@gmail.com>
2016-04-07 15:01:45 +0200

    Wip

--- inst/paper_comparison_rosmalen_aira.R
+++ inst/paper_comparison_rosmalen_aira.R
@@ -1,20 +1,25 @@
 library('aira')
 library('xtable')
 source('inst/generate_test_functions.R')
+
+horizon <<- 10
 run_aira <- function(variable_to_shock, variable_to_respond, model, name) {
-  aira <- Aira$new(bootstrap_iterations = bootstrap_iterations, horizon= 10, var_model = model,
+  aira <- Aira$new(bootstrap_iterations = bootstrap_iterations, horizon= horizon, var_model = model,
                    orthogonalize= TRUE, reverse_order=FALSE) # Reverse order is order 2
   set_exo(model)
-
+  filename <- paste("inst/output/", name, "_", variable_to_shock,"_",variable_to_respond, ".pdf", sep="")
+  pdf(file=filename, width=7, height=5)
   result <- aira$determine_length_of_effect(variable_name = variable_to_shock,
                                             response = variable_to_respond,
                                             measurement_interval = 24*60,
                                             first_effect_only = FALSE, plot_results=TRUE)
   title(name)
+  dev.off()
+  # Crop the PDF
+  system(paste('pdfcrop', filename, filename, sep=" "))
 
   # We expect the effect to be between 1.5 and three days
-  result <- result / (24 * 60)
-  print(paste(name, ':', result, 'days (', variable_to_shock, 'on', variable_to_respond,')'))
+  print(paste(name, ':', result, 'minutes (', variable_to_shock, 'on', variable_to_respond,')'))
   result
 }
 
@@ -76,8 +81,9 @@ row_names <- c('pp1','pp2','pp4','pp5')
 col_names <- c('A>D','D>A')
 
 our_results <- matrix(c(pp1,pp2,pp4,pp5),ncol=length(pp1), byrow=TRUE)
+our_results_days <- our_results / (24 * 60)
+our_results <- matrix(paste(round(our_results, 1), ' minutes (', round(our_results_days, 1), ' days)', sep=""), ncol=length(pp1))
 
-our_results <- round(our_results,1)
 rownames(our_results) <- row_names
 colnames(our_results) <- col_names
 # A > D, D > A
@@ -87,11 +93,10 @@ their_results_order_1 <- matrix(c(5,0,
                                   0,1), ncol=length(pp1), byrow = TRUE)
 
 
-# TODO: Get the correct numbers in here:
-their_results_order_2 <- matrix(c(3,1, # the 3 is 1 + 2 (1 at lag 1, 2 at lag 3,4)
-                          00,2,
-                          00,7,
-                          00,1), ncol=length(pp1), byrow = TRUE)
+#their_results_order_2 <- matrix(c(3,1, # the 3 is 1 + 2 (1 at lag 1, 2 at lag 3,4)
+#                          00,2,
+#                          00,7,
+#                          00,1), ncol=length(pp1), byrow = TRUE)
 
 rownames(their_results_order_1) <- row_names
 rownames(their_results_order_2) <- row_names
@@ -101,14 +106,15 @@ colnames(their_results_order_2) <- col_names
 # Print results in Latex table
 x <- data.frame(
   mine_som_beweg_uur = our_results,
-  their_som_beweg_uur_order1 = their_results_order_1,
-  their_som_beweg_uur_order2 = their_results_order_2
+  their_som_beweg_uur_order1 = their_results_order_1
+  #their_som_beweg_uur_order2 = their_results_order_2
 )
 
 x
 
 
-names(x) <- c("A $\\rightarrow$ D (aira)", "D $\\rightarrow$ A (aira)", "A $\\rightarrow$ D (Rosmalen 1)", "D $\\rightarrow$ A (Rosmalen 1)", "A $\\rightarrow$ D (Rosmalen 2)", "D $\\rightarrow$ A (Rosmalen 2)")
+column_names <- c("A $\\rightarrow$ D (aira)", "D $\\rightarrow$ A (aira)", "A $\\rightarrow$ D (Rosmalen 1)", "D $\\rightarrow$ A (Rosmalen 1)", "A $\\rightarrow$ D (Rosmalen 2)", "D $\\rightarrow$ A (Rosmalen 2)")
+names(x) <- column_names[1:4]
 
 print(x)
 
@@ -119,3 +125,4 @@ print(table,
       sanitize.text.function=function(str)gsub(" "," ",str,fixed=TRUE),
       floating=TRUE,
       booktabs=TRUE, floating.environment = 'table*')
+
