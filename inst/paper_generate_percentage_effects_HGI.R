c20df31854f43c8deb49630fac5bc8109a7def7a
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-22 15:43:30 +0200

    Changed paper creation tool, also creates the itemize now

--- inst/paper_generate_percentage_effects_HGI.R
+++ inst/paper_generate_percentage_effects_HGI.R
@@ -10,10 +10,10 @@ test_model <- function(model, negative_variables) {
   improve_activity <- aira$determine_percentage_effect('beweging', 10)
   improve_ontspanning <- aira$determine_percentage_effect('ontspanning', 10)
   data.frame(
-    improve_onrust = improve_onrust,
-    improve_activity = improve_activity,
-    improve_ontspanning = improve_ontspanning
-  )
+             improve_onrust = improve_onrust,
+             improve_activity = improve_activity,
+             improve_ontspanning = improve_ontspanning
+             )
 }
 
 negative_variables <- c('onrust')
@@ -46,25 +46,25 @@ print(p110478)
 print(p100713)
 
 decrease_onrust <- data.frame(
-  'decrease.feeling.nervous.by.changing.activity' = c(p100849$improve_onrust.beweging.percentage, p100551$improve_onrust.beweging.percentage, p112098$improve_onrust.beweging.percentage, p110478$improve_onrust.beweging.percentage, p100713$improve_onrust.beweging.percentage),
-  'decrease.feeling.nervous.by.changing.relaxation' = c(p100849$improve_onrust.ontspanning.percentage, p100551$improve_onrust.ontspanning.percentage, p112098$improve_onrust.ontspanning.percentage, p110478$improve_onrust.ontspanning.percentage, p100713$improve_onrust.ontspanning.percentage)
-  )
+                              'decrease.feeling.nervous.by.changing.activity' = c(p100849$improve_onrust.beweging.percentage, p100551$improve_onrust.beweging.percentage, p112098$improve_onrust.beweging.percentage, p110478$improve_onrust.beweging.percentage, p100713$improve_onrust.beweging.percentage),
+                              'decrease.feeling.nervous.by.changing.relaxation' = c(p100849$improve_onrust.ontspanning.percentage, p100551$improve_onrust.ontspanning.percentage, p112098$improve_onrust.ontspanning.percentage, p110478$improve_onrust.ontspanning.percentage, p100713$improve_onrust.ontspanning.percentage)
+                              )
 
 increase_activity <- data.frame(
-  'increase.activity.by.changing.feeling.nervous' = c(p100849$improve_activity.onrust.percentage, p100551$improve_activity.onrust.percentage, p112098$improve_activity.onrust.percentage, p110478$improve_activity.onrust.percentage, p100713$improve_activity.onrust.percentage),
-  'increase.activity.by.changing.relaxation' = c(p100849$improve_activity.ontspanning.percentage, p100551$improve_activity.ontspanning.percentage, p112098$improve_activity.ontspanning.percentage, p110478$improve_activity.ontspanning.percentage, p100713$improve_activity.ontspanning.percentage)
-)
+                                'increase.activity.by.changing.feeling.nervous' = c(p100849$improve_activity.onrust.percentage, p100551$improve_activity.onrust.percentage, p112098$improve_activity.onrust.percentage, p110478$improve_activity.onrust.percentage, p100713$improve_activity.onrust.percentage),
+                                'increase.activity.by.changing.relaxation' = c(p100849$improve_activity.ontspanning.percentage, p100551$improve_activity.ontspanning.percentage, p112098$improve_activity.ontspanning.percentage, p110478$improve_activity.ontspanning.percentage, p100713$improve_activity.ontspanning.percentage)
+                                )
 
 increase_relaxation <- data.frame(
-  'increase.relaxation.by.changing.feeling.nervous.' = c(p100849$improve_ontspanning.onrust.percentage, p100551$improve_ontspanning.onrust.percentage, p112098$improve_ontspanning.onrust.percentage, p110478$improve_ontspanning.onrust.percentage, p100713$improve_ontspanning.onrust.percentage),
-  'increase.activity.by.changing.activity' = c(p100849$improve_ontspanning.beweging.percentage, p100551$improve_ontspanning.beweging.percentage, p112098$improve_ontspanning.beweging.percentage, p110478$improve_ontspanning.beweging.percentage, p100713$improve_ontspanning.beweging.percentage)
-)
+                                  'increase.relaxation.by.changing.feeling.nervous.' = c(p100849$improve_ontspanning.onrust.percentage, p100551$improve_ontspanning.onrust.percentage, p112098$improve_ontspanning.onrust.percentage, p110478$improve_ontspanning.onrust.percentage, p100713$improve_ontspanning.onrust.percentage),
+                                  'increase.relaxation.by.changing.activity' = c(p100849$improve_ontspanning.beweging.percentage, p100551$improve_ontspanning.beweging.percentage, p112098$improve_ontspanning.beweging.percentage, p110478$improve_ontspanning.beweging.percentage, p100713$improve_ontspanning.beweging.percentage)
+                                  )
 
 x <- rbind(
-   t(decrease_onrust),
-   t(increase_activity),
-   t(increase_relaxation)
-)
+           t(decrease_onrust),
+           t(increase_activity),
+           t(increase_relaxation)
+           )
 x <- t(x)
 
 rownames(x)<- paste('Person',1:dim(x)[1])
@@ -86,18 +86,30 @@ for(person in dimnames(x)[[1]]) {
   }
 }
 
+# Remove effects > 1000%
+x[abs(x) > 1000] <- Inf
 x[x != Inf] <- paste(round(x[x != Inf],2), '\\%', sep='')
 x[x == Inf] <- '$\\infty$'
 
 #
 table <- xtable(x, label="tab:percentage_effects_in_aira",
-                 caption='Effects of Feeling less nervous, relaxation and activity on well-being', digits = 3)
+                caption='Effects of Feeling less nervous, relaxation and activity on well-being', digits = 3)
 print(table,
-       file='inst/output/tab_percentage_effects_in_aira.tex',
-       sanitize.text.function=function(str)gsub(" "," ",str,fixed=TRUE),
-       floating=TRUE,
-       booktabs=TRUE, floating.environment = 'table*')
+      file='inst/output/tab_percentage_effects_in_aira.tex',
+      sanitize.text.function=function(str)gsub(" "," ",str,fixed=TRUE),
+      floating=TRUE,
+      booktabs=TRUE, floating.environment = 'table*')
 
 
 print(x)
-
+sink('inst/output/item_percentage_effects_in_aira.tex')
+print("\\begin{itemize}")
+for (r in 1:nrow(x)) {
+  for (c in 1:ncol(x)) {
+    if(x[r,c] == "$\\infty$") next
+    res = paste("\\item", rownames(x)[r], 'can', colnames(x)[c],'by', x[r,c])
+    print(res)
+  }
+}
+print("\\end{itemize}")
+sink()
--- inst/paper_sine_wave.R
+++ inst/paper_sine_wave.R
@@ -9,16 +9,20 @@ for (j in x) {
 
 output <- output / max(output) * 2
 
+noise <- sin(seq(0.01,10,0.01))
 x <- x / max(x/10)
 
 y.loess <- loess(y ~ x, span=0.1, data.frame(x=x, y=output))
 y.predict <- predict(y.loess, data.frame(x=x))
 
 
+offset = .5
 test_data <<- data.frame(
-  x = x,
-  out = y.predict
-)
+                         x = x,
+                         out = y.predict,
+                         out_upper = y.predict + (offset - noise/2.5),
+                         out_lower = y.predict - (offset)
+                         )
 
 plotje <- ggplot(data=test_data,aes(x=x, y=out), show_guide = FALSE) +
   geom_line(aes(y=out, colour='gray')) +
@@ -34,6 +38,10 @@ plotje <- ggplot(data=test_data,aes(x=x, y=out), show_guide = FALSE) +
   scale_x_continuous(breaks = round(seq(min(test_data$x), max(test_data$x), by = 1),1))+
   theme(panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank()) +
   theme(panel.grid.major.y = element_line(colour="gray"), panel.grid.minor.y = element_blank()) +
+  geom_line(aes(y=out_upper),colour='#000000', linetype=2, alpha=0.3) +
+  geom_line(aes(y=out_lower),colour='#000000', linetype=2, alpha=0.3) +
+  geom_ribbon(data = subset(test_data, out_upper < 0), aes(ymin = 0, ymax = out_upper), fill = '#000000', alpha=0.3) +
+  geom_ribbon(data = subset(test_data, out_lower > 0), aes(ymin = out_lower, ymax = 0), fill = '#000000', alpha=0.3)+
   theme(panel.border = element_blank()) +
   theme(axis.line = element_blank())
 
